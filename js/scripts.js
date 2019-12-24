5cc9eb3dd96558a1f5dcf47984af992bd8b2ceb8
Ariska28 <55794499+Ariska28@users.noreply.github.com>
2019-12-24 12:20:14 +0300

    menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,18 @@
-$().ready(function () {
-  $().ready(function(){
-    $('.slick__block').slick({
-      prevArrow: $('.slick__prewarrow'),
-      nextArrow: $('.slick__nextarrow'),
-      centerMode: true,
-      centerPadding: '100px',
-      slidesToShow: 3,
-      dots: true,
-      responsive: [
-        {
-          breakpoint: 1000,
-          settings: {
-            slidesToShow: 2,
-            dots: true,
-            centerPadding: '50px',
-            centerMode: true,
-            infinite: true,
-          }
-        },
-        {
-          breakpoint: 800,
-          settings: {
-            slidesToShow: 1,
-            dots: true,
-            centerPadding: '0px',
-            centerMode: true,
-            infinite: true,
-          }
-        }
-      ]
+document.addEventListener('DOMContentLoaded', function () {
+  function openMenu() {
+    const menuToggler = document.querySelector('.js-menu-btn');
+    const mobileNavContainer = document.querySelector('.header-list');
+    console.log(menuToggler);
+    console.log(mobileNavContainer);
+
+    menuToggler.addEventListener('click', function () {
+      menuToggler.classList.toggle('header-icon-active');
+      mobileNavContainer.classList.toggle('mobile-nav--active');
     });
-  });
-  
-  });
+
+
+  };
+
+  openMenu();
+});
+
