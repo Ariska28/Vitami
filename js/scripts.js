8152793702896a7e00bfef4f8ed77e5c86130558
Ariska28 <55794499+Ariska28@users.noreply.github.com>
2019-12-24 18:59:05 +0300

    reit

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,20 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuToggler = document.querySelector('.js-menu-btn');
     const mobileNavContainer = document.querySelector('.header-list');
     const mobileNavTools = document.querySelector('.header-tools');
+    const mobileMain = document.querySelector('.main');
+    const mobileFooter = document.querySelector('.footer');
     console.log(menuToggler);
     console.log(mobileNavContainer);
     console.log(mobileNavTools);
+    console.log(mobileMain);
+    console.log(mobileFooter);
 
     menuToggler.addEventListener('click', function () {
       menuToggler.classList.toggle('header-icon-active');
       mobileNavContainer.classList.toggle('mobile-nav--active');
       mobileNavTools.classList.toggle('mobile-nav--active1');
+      mobileMain.classList.toggle('main--hidden');
+      mobileFooter.classList.toggle('footer--hidden');
     });
 
 
