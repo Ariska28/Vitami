f48964dc203d21f2e163f83b60fac78a55d56f5d
Ariska28 <55794499+Ariska28@users.noreply.github.com>
2019-12-24 17:50:40 +0300

    menu

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,15 @@ document.addEventListener('DOMContentLoaded', function () {
   function openMenu() {
     const menuToggler = document.querySelector('.js-menu-btn');
     const mobileNavContainer = document.querySelector('.header-list');
+    const mobileNavTools = document.querySelector('.header-tools');
     console.log(menuToggler);
     console.log(mobileNavContainer);
+    console.log(mobileNavTools);
 
     menuToggler.addEventListener('click', function () {
       menuToggler.classList.toggle('header-icon-active');
       mobileNavContainer.classList.toggle('mobile-nav--active');
+      mobileNavTools.classList.toggle('mobile-nav--active1');
     });
 
 
