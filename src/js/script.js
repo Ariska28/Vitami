01e1486a3dfa2f959949b2d6f53de0b05831a6ef
Ariska28 <55794499+Ariska28@users.noreply.github.com>
2020-01-04 15:29:59 +0300

    menu

--- src/js/script.js
+++ src/js/script.js
@@ -3,23 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuToggler = document.querySelector('.js-menu-btn');
     const mobileNavContainer = document.querySelector('.header-navigation');
     const mobileNavTools = document.querySelector('.header-tools');
-    const mobileMain = document.querySelector('.main');
-    const mobileFooter = document.querySelector('.footer');
-    const mobileHeader = document.querySelector('.header');
     console.log(menuToggler);
     console.log(mobileNavContainer);
     console.log(mobileNavTools);
-    console.log(mobileMain);
-    console.log(mobileFooter);
-    console.log(mobileHeader);
 
     menuToggler.addEventListener('click', function () {
       menuToggler.classList.toggle('header-icon-active');
       mobileNavContainer.classList.toggle('mobile-nav--active');
       mobileNavTools.classList.toggle('mobile-nav--active1');
-      mobileMain.classList.toggle('main--hidden');
-      mobileFooter.classList.toggle('footer--hidden');
-      mobileHeader.classList.toggle('header--white');
     });
 
 
